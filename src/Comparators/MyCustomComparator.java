23b55214635cdd97f4c3cec457b3443e30f665f7
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-03 23:09:23 +0500

    Comparator Interface

---
+++ src/Comparators/MyCustomComparator.java
@@ -0,0 +1,12 @@
+package Comparators;
+
+import java.util.Comparator;
+
+public class MyCustomComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+
+    }
+}
